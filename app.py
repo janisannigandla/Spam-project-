8e2abd6378d9658d521e609a0a9d1ba93cacd339
janisannigandla <janisannigandla226@gmail.com>
2026-06-17 19:42:57 +0530

    Fix Render port binding

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+print("STEP 1")
 from flask import Flask,render_template,jsonify,request, redirect, url_for, flash
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from flask_bcrypt import Bcrypt
